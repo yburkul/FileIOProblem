8fa2a0a1307f8fcf5faeb41d4d1804d255073ff6
yburkul <yburkul123@gmail.com>
2022-04-14 20:01:25 +0530

    Added:File IO

---
+++ FileIOPractice/FileIOPractice/PracticeFile.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIOPractice
+{
+   public class PracticeFile
+   {
+        public void FileExist()
+        {
+            string InputFile = @"D:\Bridglabz\FileIOPractice\Sample.txt";
+            if (File.Exists(InputFile))
+            {
+                Console.WriteLine("File Exist");
+            }
+            else
+            {
+                Console.WriteLine("File is not found");
+            }
+        }
+        public void ReadAllText()
+        {
+            string filePath = @"D:\Bridglabz\FileIOPractice\Sample.txt";
+            string read = File.ReadAllText(filePath);
+            Console.WriteLine(read);
+        }
+        public void ReadAllLine()
+        {
+            string Path = @"D:\Bridglabz\FileIOPractice\Sample.txt";
+            string[] readline = File.ReadAllLines(Path);
+            foreach (string line in readline)
+            {
+                Console.WriteLine(line);
+            }
+        }
+        public void ReadAllBytes()
+        {
+            string Path = @"D:\Bridglabz\FileIOPractice\Sample.txt";
+            byte[] readBytes = File.ReadAllBytes(Path);
+            foreach (byte b in readBytes)
+            {
+                Console.WriteLine(b);
+            }
+        }
+        public void WriteAllText()
+        {
+            string filePath = @"D:\Bridglabz\FileIOPractice\Demo.txt";
+            string data = "C# Corner MVP & Microsoft MVP;";
+            File.WriteAllText(filePath, data);
+        }
+        public void ReadAllLines()
+        {
+            string filePath = @"D:\Bridglabz\FileIOPractice\Demo.txt";
+            string[] data = { "MCT", "MCPD", "MCTS", "MCSD.NET", "MCAD.NET", "CSM" };
+            File.WriteAllLines(filePath, data);
+            foreach (string line in data)
+            {
+                Console.WriteLine(line);
+            }
+        }
+        public void AppendAllText()
+        {
+            string filePath = @"D:\Bridglabz\FileIOPractice\Demo.txt";
+            string data = "Also Certified from IIT Kharagpur";
+            File.AppendAllText(filePath, data);
+        }
+        public void AppendAllLine()
+        {
+            string filePath = @"D:\Bridglabz\FileIOPractice\Demo.txt";
+            string[] otherData = { "Worked with Microsoft", "Lived in USA" };
+            File.AppendAllLines(filePath, otherData);
+        }
+        public void CopyFile()
+        {
+            string filePath = @"D:\Bridglabz\FileIOPractice\Demo.txt";
+            string copyPath = @"D:\Bridglabz\FileIOPractice\DemoSample.txt";
+            bool overWrite = true;
+            File.Copy(filePath, copyPath, overWrite);
+        }
+        public void DeleteFile()
+        {
+            string filePath = @"D:\Bridglabz\FileIOPractice\DemoSample.txt";
+            File.Delete(filePath);
+        }
+        public void GetCreationTime()
+        {
+            string sourceFilePath = @"D:\Bridglabz\FileIOPractice\Demo.txt";
+            DateTime fileCreatedOn = File.GetCreationTime(sourceFilePath);
+            Console.WriteLine(fileCreatedOn);
+        }
+        public void DirectoryName()
+        {
+            string sourceFilePath = @"D:\Bridglabz\FileIOPractice\Demo.txt";
+            FileInfo fInfo = new FileInfo(sourceFilePath);
+            string directoryPath = fInfo.DirectoryName;
+            Console.WriteLine(directoryPath);
+        }
+        public void LengthOfFile()
+        {
+            string sourceFilePath = @"D:\Bridglabz\FileIOPractice\Demo.txt";
+            FileInfo fInfo = new FileInfo(sourceFilePath);
+            long length = fInfo.Length;
+            Console.WriteLine(length);
+        }
+        public void CreateDirectory()
+        {
+            string sourceDirPath = @"D:\Bridglabz\FileIOPractice\Demo";
+            Directory.CreateDirectory(sourceDirPath);
+        }
+        public void DeleteDirectory()
+        {
+            string sourceDirPath = @"D:\Bridglabz\FileIOPractice\Demo";
+            Directory.Delete(sourceDirPath);
+        }
+   }
+}
---
+++ FileIOPractice/FileIOPractice/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIOPractice
+{
+    public class FileIO
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome in File IO");
+            PracticeFile practiceFile = new PracticeFile();
+            int input = 0;
+            do
+            {
+                Console.WriteLine("1: File Exist");
+                Console.WriteLine("2: Read All Text");
+                Console.WriteLine("3: Read all line");
+                Console.WriteLine("4: Read all bytes");
+                Console.WriteLine("5: Read Write all text");
+                Console.WriteLine("6: Read all lines");
+                Console.WriteLine("7: Append all text");
+                Console.WriteLine("8: Append All line");
+                Console.WriteLine("9: Copy File");
+                Console.WriteLine("10: Delete File");
+                Console.WriteLine("11: file creation time");
+                Console.WriteLine("12: Directory Name ");
+                Console.WriteLine("13: Length of File");
+                Console.WriteLine("14: CreateDirectory");
+                Console.WriteLine("15: Deletes a Directory ");
+                 input = int.Parse(Console.ReadLine());
+                switch (input)
+                {
+                    case 1:
+                      practiceFile.FileExist();
+                        break;
+                    case 2:
+                        practiceFile.ReadAllText();
+                        break;
+                    case 3:
+                        practiceFile.ReadAllLine();
+                        break;
+                    case 4:
+                        practiceFile.ReadAllBytes();
+                        break;
+                    case 5:
+                        practiceFile.WriteAllText();
+                        break;
+                    case 6:
+                        practiceFile.ReadAllLines();
+                        break;
+                    case 7:
+                        practiceFile.AppendAllText();
+                        break;
+                    case 8:
+                        practiceFile.AppendAllLine();
+                        break;
+                    case 9:
+                        practiceFile.CopyFile();
+                        break;
+                    case 10:
+                        practiceFile.DeleteFile();
+                        break;
+                    case 11:
+                        practiceFile.GetCreationTime();
+                        break;
+                    case 12:
+                        practiceFile.DirectoryName();
+                            break;
+                    case 13:
+                        practiceFile.LengthOfFile();
+                        break;
+                     case 14:
+                        practiceFile.CreateDirectory();
+                        break;
+                    case 15:
+                        practiceFile.DeleteDirectory();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Input");
+                        break;
+                }
+            }
+            while (input != 0);
+        }
+    }
+}
